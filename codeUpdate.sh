d6ea55a07081d9c41df6ab56422ad920e37f2930
abragagn <alberto.bragagnolo@pd.infn.it>
2019-07-03 11:53:00 +0200

    Update

--- codeUpdate.sh
+++ codeUpdate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm ./albertoCode.tar.gz
 tar -czvf ./albertoCode.tar.gz ./PDAnalysis/
-scp ./albertoCode* abragagn@t2-ui-12:/lustre/cmswork/abragagn/BPH/
-#scp -2 -P 4444 -oNoHostAuthenticationForLocalhost=yes ./albertoCode.tar.gz  abragagn@localhost:/lustre/cmswork/abragagn/BPH/
+#scp ./albertoCode* abragagn@t2-ui-12:/lustre/cmswork/abragagn/BPH/
+scp -2 -P 4444 -oNoHostAuthenticationForLocalhost=yes ./albertoCode.tar.gz  abragagn@localhost:/lustre/cmswork/abragagn/BPH/
 
 git commit -a -m "Update"
 git push
